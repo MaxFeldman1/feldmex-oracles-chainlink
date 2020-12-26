dea215830f47efa0798de7a1512f392d8a741eea
MaxFeldman1 <mackx@protonmail.com>
2020-12-25 23:02:07 -0600

    access to all data through the OracleContainer contract

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -1,45 +1,49 @@
-pragma solidity >=0.6.0 <0.7.0;
+pragma solidity >=0.6.0;
 
-import "@chainlink/contracts/src/v0.6/interfaces/AggregatorInterface.sol";
-import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
-import "@chainlink/contracts/src/v0.6/AggregatorFacade.sol";
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV2V3Interface.sol";
+import "./IAggregatorFacade.sol";
+import "./IFeldmexOracle.sol";
 
-contract Oracle {
+contract Oracle is IFeldmexOracle {
 
     address aggregatorAddress;
     address public fluxAggregatorAddress;
     
-    AggregatorInterface ai2;
+    AggregatorV2V3Interface ai;
 
     constructor (address _aggregatorAddress) public {
         aggregatorAddress = _aggregatorAddress;
-        fluxAggregatorAddress = address(AggregatorFacade(_aggregatorAddress).aggregator());
-        ai2 = AggregatorInterface(fluxAggregatorAddress);
+        fluxAggregatorAddress = address(IAggregatorFacade(_aggregatorAddress).aggregator());
+        ai = AggregatorV2V3Interface(fluxAggregatorAddress);
     }
 
-    function fetchRoundBehind(uint80 _roundId) public view returns (uint, uint80) {
-        uint timestamp = ai2.getTimestamp(_roundId);
+    function fetchRoundBehind(uint80 _roundId) internal view returns (uint, uint80) {
+        uint timestamp = ai.getTimestamp(_roundId);
         while (timestamp == 0 && _roundId != 0) {
             _roundId--;
-            timestamp = ai2.getTimestamp(_roundId);
+            timestamp = ai.getTimestamp(_roundId);
         }
         return (timestamp, _roundId);
     }
 
-    function fetchSpotAtTime(uint timestamp) public view returns (uint price) {
+    function fetchSpotAtTime(uint timestamp) external view override returns (uint price) {
         //we can safely assume that the price will never be negative
-        price = uint(ai2.getAnswer(fetchRoundAtTimestamp(timestamp)));
+        price = uint(ai.getAnswer(fetchRoundAtTimestamp(timestamp)));
     }
 
     function fremostRoundWithSameTimestamp(uint _roundId) internal view returns (uint) {
-        uint timestamp = ai2.getTimestamp(_roundId);
+        uint timestamp = ai.getTimestamp(_roundId);
         _roundId++;
-        while (timestamp == ai2.getTimestamp(_roundId)) _roundId++;
+        while (timestamp == ai.getTimestamp(_roundId)) _roundId++;
         return _roundId-1;
     }
 
-    function fetchRoundAtTimestamp(uint timestamp) public view returns (uint) {
-        uint80 latest = uint80(ai2.latestRound());
+    function decimals() external view override returns (uint8) {
+        return ai.decimals();
+    }
+
+    function fetchRoundAtTimestamp(uint timestamp) public view override returns (uint) {
+        uint80 latest = uint80(ai.latestRound());
         (uint fetchedTime, uint80 fetchedRound) = fetchRoundBehind(latest);
         if (timestamp >= fetchedTime) return fremostRoundWithSameTimestamp(fetchedRound);
         latest = fetchedRound;
---
+++ contracts/OracleContainer.sol
@@ -0,0 +1,97 @@
+pragma solidity >=0.6.0;
+pragma experimental ABIEncoderV2;
+import "./Ownable.sol";
+import "./Oracle.sol";
+import "./IFeldmexOracle.sol";
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV2V3Interface.sol";
+
+contract OracleContainer is Ownable {
+
+	struct Info {
+		address aggregatorAddress;
+		address oracleAddress;
+	}
+
+	mapping(address => string) public TickerSymbols;
+
+	mapping(string => Info) public PairInfo;
+
+	function oracleAddresses(address _strikeAssetAddress, address _underlyingAssetAddress) public view returns (address) {
+		return PairInfo[string(abi.encodePacked(TickerSymbols[_strikeAssetAddress], '/', TickerSymbols[_underlyingAssetAddress]))].oracleAddress;
+	}
+
+	function deploy(address _strikeAssetAddress, address _underlyingAssetAddress) public {
+		require(_underlyingAssetAddress != _strikeAssetAddress, "underlying asset must not be the same as strike asset");
+		Info memory info = PairInfo[string(abi.encodePacked(TickerSymbols[_strikeAssetAddress], '/', TickerSymbols[_underlyingAssetAddress]))];
+		require(info.aggregatorAddress != address(0), "chainlink aggregator must exist to create options chain");
+		require(info.oracleAddress == address(0), "cannot deploy oracle that already exists");
+		info.oracleAddress = address(new Oracle(info.aggregatorAddress));
+	}
+
+	function addTickers(address[] memory _assetAddresses, string[] memory _tickerSymbols) public onlyOwner {
+		uint length = _assetAddresses.length;
+		require(length == _tickerSymbols.length);
+		for (uint i = 0; i < length; i++) {
+			require(bytes(TickerSymbols[_assetAddresses[i]]).length == 0);
+			TickerSymbols[_assetAddresses[i]] = _tickerSymbols[i];
+		}
+	}
+
+	function addAggregators(string[] memory _pairTicker, address[] memory _aggregators) public onlyOwner {
+		uint length = _pairTicker.length;
+		require(length == _aggregators.length);
+		for (uint i = 0; i < length; i++) {
+			require(bytes(_pairTicker[i])[0] != "/");
+			require(bytes(_pairTicker[i])[_pairTicker.length-1] != "/");
+			PairInfo[_pairTicker[i]].aggregatorAddress = _aggregators[i];
+		}
+	}
+
+	function tokensToLatestPrice(address _strikeAssetAddress, address _underlyingAssetAddress) public view returns (uint spot, uint8 decimals) {
+		address aggregatorAddress = PairInfo[string(abi.encodePacked(TickerSymbols[_strikeAssetAddress], '/', TickerSymbols[_underlyingAssetAddress]))].aggregatorAddress;
+		//when flip == true we need to return 1/price fetched
+		bool flip = aggregatorAddress == address(0);
+		if (flip) {
+			aggregatorAddress = PairInfo[string(abi.encodePacked(TickerSymbols[_underlyingAssetAddress], '/', TickerSymbols[_strikeAssetAddress]))].aggregatorAddress;
+			require(aggregatorAddress != address(0));
+		}
+		//we can safely assume that the spot will never be negative and that a conversion to uint will be safe.
+		spot = uint(AggregatorV2V3Interface(aggregatorAddress).latestAnswer());
+		decimals = AggregatorV2V3Interface(aggregatorAddress).decimals();
+		if (flip) spot = 10**uint(2*decimals) / spot;
+	}
+
+
+	function phraseToLatestPrice(string memory _phrase) public view returns (uint spot, uint8 decimals) {
+		address aggregatorAddress = PairInfo[_phrase].aggregatorAddress;
+		require(aggregatorAddress != address(0));
+		//we can safely assume that the spot will never be negative and that a conversion to uint will be safe.
+		spot = uint(AggregatorV2V3Interface(aggregatorAddress).latestAnswer());
+		decimals = AggregatorV2V3Interface(aggregatorAddress).decimals();		
+	}
+
+
+	function tokensToHistoricalPrice(address _strikeAssetAddress, address _underlyingAssetAddress, uint _timestamp) public view returns (uint spot, uint8 decimals) {
+		address oracleAddress = PairInfo[string(abi.encodePacked(TickerSymbols[_strikeAssetAddress], '/', TickerSymbols[_underlyingAssetAddress]))].oracleAddress;
+		//when flip == true we need to return 1/price fetched
+		bool flip = oracleAddress == address(0);
+		if (flip) {
+			oracleAddress = PairInfo[string(abi.encodePacked(TickerSymbols[_underlyingAssetAddress], '/', TickerSymbols[_strikeAssetAddress]))].oracleAddress;
+			require(oracleAddress != address(0));
+		}
+		spot = IFeldmexOracle(oracleAddress).fetchSpotAtTime(_timestamp);
+		decimals = IFeldmexOracle(oracleAddress).decimals();
+		if (flip) spot = 10**uint(2*decimals) / spot;
+	}
+
+
+	function phraseToHistoricalPrice(string memory _phrase, uint _timestamp) public view returns (uint spot, uint8 decimals) {
+		address oracleAddress = PairInfo[_phrase].oracleAddress;
+		require(oracleAddress != address(0));
+		spot = IFeldmexOracle(oracleAddress).fetchSpotAtTime(_timestamp);
+		decimals = IFeldmexOracle(oracleAddress).decimals();
+	}
+
+
+}
