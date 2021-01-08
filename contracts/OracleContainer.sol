cfb6835aabda58557f8be7ee9c68dfc8022c7e8b
MaxFeldman1 <mackx@protonmail.com>
2021-01-08 07:13:55 -0600

    remove fetch price by token addresses from master branch

--- contracts/OracleContainer.sol
+++ contracts/OracleContainer.sol
@@ -15,30 +15,13 @@ contract OracleContainer is Ownable, IOracleContainer {
 		address oracleAddress;
 	}
 
-	mapping(address => string) public TickerSymbols;
-
 	mapping(string => Info) public PairInfo;
 
-	function oracleAddresses(address _strikeAssetAddress, address _underlyingAssetAddress) public view returns (address) {
-		return PairInfo[string(abi.encodePacked(TickerSymbols[_strikeAssetAddress], '/', TickerSymbols[_underlyingAssetAddress]))].oracleAddress;
-	}
-
-	function deploy(address _strikeAssetAddress, address _underlyingAssetAddress) public {
-		require(_underlyingAssetAddress != _strikeAssetAddress, "underlying asset must not be the same as strike asset");
-		string memory phrase = string(abi.encodePacked(TickerSymbols[_strikeAssetAddress], '/', TickerSymbols[_underlyingAssetAddress]));
-		Info memory info = PairInfo[phrase];
+	function deploy(string memory _phrase) public {
+		Info memory info = PairInfo[_phrase];
 		require(info.baseAggregatorAddress != address(0), "chainlink aggregator must exist to create options chain");
 		require(info.oracleAddress == address(0), "cannot deploy oracle that already exists");
-		PairInfo[phrase].oracleAddress = address(new Oracle(info.baseAggregatorAddress));
-	}
-
-	function addTickers(address[] memory _assetAddresses, string[] memory _tickerSymbols) public onlyOwner {
-		uint length = _assetAddresses.length;
-		require(length == _tickerSymbols.length);
-		for (uint i = 0; i < length; i++) {
-			require(bytes(TickerSymbols[_assetAddresses[i]]).length == 0);
-			TickerSymbols[_assetAddresses[i]] = _tickerSymbols[i];
-		}
+		PairInfo[_phrase].oracleAddress = address(new Oracle(info.baseAggregatorAddress));
 	}
 
 	function addAggregators(string[] memory _pairTicker, address[] memory _facades) public onlyOwner {
@@ -51,24 +34,6 @@ contract OracleContainer is Ownable, IOracleContainer {
 		}
 	}
 
-	function tokensToLatestPrice(address _strikeAssetAddress, address _underlyingAssetAddress) external view override returns (uint spot, uint8 decimals) {
-		address baseAggregatorAddress = PairInfo[string(abi.encodePacked(TickerSymbols[_strikeAssetAddress], '/', TickerSymbols[_underlyingAssetAddress]))].baseAggregatorAddress;
-		//when flip == true we need to return 1/price fetched
-		bool flip = baseAggregatorAddress == address(0);
-		if (flip) {
-			baseAggregatorAddress = PairInfo[string(abi.encodePacked(TickerSymbols[_underlyingAssetAddress], '/', TickerSymbols[_strikeAssetAddress]))].baseAggregatorAddress;
-			require(baseAggregatorAddress != address(0));
-		}
-		//we can safely assume that the spot will never be negative and that a conversion to uint will be safe.
-		spot = uint(AggregatorV2V3Interface(baseAggregatorAddress).latestAnswer());
-		decimals = AggregatorV2V3Interface(baseAggregatorAddress).decimals();
-		if (flip) {
-			spot = 10**uint(18+decimals) / spot;
-			decimals = 18;
-		}
-	}
-
-
 	function phraseToLatestPrice(string calldata _phrase) external view override returns (uint spot, uint8 decimals) {
 		address baseAggregatorAddress = PairInfo[_phrase].baseAggregatorAddress;
 		require(baseAggregatorAddress != address(0));
@@ -76,24 +41,6 @@ contract OracleContainer is Ownable, IOracleContainer {
 		spot = uint(AggregatorV2V3Interface(baseAggregatorAddress).latestAnswer());
 		decimals = AggregatorV2V3Interface(baseAggregatorAddress).decimals();
 	}
-
-
-	function tokensToHistoricalPrice(address _strikeAssetAddress, address _underlyingAssetAddress, uint _timestamp) external view override returns (uint spot, uint8 decimals) {
-		address oracleAddress = PairInfo[string(abi.encodePacked(TickerSymbols[_strikeAssetAddress], '/', TickerSymbols[_underlyingAssetAddress]))].oracleAddress;
-		//when flip == true we need to return 1/price fetched
-		bool flip = oracleAddress == address(0);
-		if (flip) {
-			oracleAddress = PairInfo[string(abi.encodePacked(TickerSymbols[_underlyingAssetAddress], '/', TickerSymbols[_strikeAssetAddress]))].oracleAddress;
-			require(oracleAddress != address(0));
-		}
-		spot = IFeldmexOracle(oracleAddress).fetchSpotAtTime(_timestamp);
-		decimals = IFeldmexOracle(oracleAddress).decimals();
-		if (flip) {
-			spot = 10**uint(18+decimals) / spot;
-			decimals = 18;
-		}
-	}
-
 
 	function phraseToHistoricalPrice(string calldata _phrase, uint _timestamp) external view override returns (uint spot, uint8 decimals) {
 		address oracleAddress = PairInfo[_phrase].oracleAddress;
--- test/OracleContainer.js
+++ test/OracleContainer.js
@@ -33,17 +33,12 @@ contract('OracleContainer', async () => {
 			//advance 100 seconds between rounds
 			await helper.advanceTime(100);
 		}
-	});
-
-	it('add tickers', async () => {
-		Ticker0 = "BTC";
-		Ticker1 = "USD";
-		await containerInstance.addTickers([asset0.address, asset1.address], [Ticker0, Ticker1]);
-		assert.equal(await containerInstance.TickerSymbols(asset0.address), Ticker0, "correct ticker for asset0");
-		assert.equal(await containerInstance.TickerSymbols(asset1.address), Ticker1, "correct ticker for asset1");
+		firstTime = await baseAggregatorInstance.getTimestamp(0);
 	});
 
 	it('add aggregators', async () => {
+		Ticker0 = "BTC";
+		Ticker1 = "USD";
 		phrase = Ticker0+"/"+Ticker1;
 		await containerInstance.addAggregators([phrase], [facadeInstance.address]);
 		info = await containerInstance.PairInfo(phrase);
@@ -52,42 +47,17 @@ contract('OracleContainer', async () => {
 	});
 
 	it('deploy Oracle', async () => {
-		await containerInstance.deploy(asset0.address, asset1.address);
+		await containerInstance.deploy(phrase);
 		info = await containerInstance.PairInfo(phrase);
 		assert.equal(info.baseAggregatorAddress, baseAggregatorInstance.address, "info.baseAggregatorAddress is correct");
 		assert.notEqual(info.oracleAddress, defaultAddress, "info.oracleAddress should be non null");
 		oracleInstance = await Oracle.at(info.oracleAddress);
 	});
 
-	it('tokens to latest price flip:false', async () => {
-		let res = await containerInstance.tokensToLatestPrice(asset0.address, asset1.address);
-		assert.equal(res.spot.toString(), prices[prices.length-1], "correct value of spot returned from tokensToLatestPrice()");
-		assert.equal(res.decimals.toString(), decimals, "correct value of decimals returned from tokensToLatestPrice()");
-	});
-
-	it('tokens to latest price flip:true', async () => {
-		let res = await containerInstance.tokensToLatestPrice(asset1.address, asset0.address);
-		assert.equal(res.spot.toString(), ((new BN("10")).pow(new BN(18 + parseInt(decimals)))).div(new BN(prices[prices.length-1])).toString(), "correct value of spot returned from tokensToLatestPrice()");
-		assert.equal(res.decimals.toString(), "18", "correct value of decimals returned from tokensToLatestPrice()");
-	});
-
 	it('phrase to latest price', async () => {
 		let res = await containerInstance.phraseToLatestPrice(phrase);
 		assert.equal(res.spot.toString(), prices[prices.length-1], "correct value of spot returned from phraseToHistoricalPrice()");
 		assert.equal(res.decimals.toString(), decimals, "correct value of decimals returned from phraseToHistoricalPrice()");
-	});
-
-	it('tokens to historical price flip:false', async () => {
-		firstTime = await baseAggregatorInstance.getTimestamp(0);
-		let res = await containerInstance.tokensToHistoricalPrice(asset0.address, asset1.address, firstTime.toString());
-		assert.equal(res.spot.toString(), prices[0], "correct value of spot retuend from tokensToHistoricalPrice()");
-		assert.equal(res.decimals.toString(), decimals, "correct value of decimals retuend from tokensToHistoricalPrice()");
-	});
-
-	it('tokens to historical price flip:true', async () => {
-		let res = await containerInstance.tokensToHistoricalPrice(asset1.address, asset0.address, firstTime.toString());
-		assert.equal(res.spot.toString(), ((new BN("10")).pow(new BN(18 + parseInt(decimals)))).div(new BN(prices[0])).toString(), "correct value of spot retuend from tokensToHistoricalPrice()");
-		assert.equal(res.decimals.toString(), "18", "correct value of decimals retuend from tokensToHistoricalPrice()");
 	});
 
 	it('phrase to historical price', async () => {
